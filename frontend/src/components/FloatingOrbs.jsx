7cf5e9aa42dc9ef4351b9079352b60c56af44a1c
gyuvakishorereddy <g.yuvakishorereddy@gmail.com>
2026-03-01 00:01:23 +0530

    jsx file

---
+++ frontend/src/components/FloatingOrbs.jsx
@@ -0,0 +1,15 @@
+import './FloatingOrbs.css'
+
+function FloatingOrbs() {
+  return (
+    <div className="floating-orbs">
+      <div className="orb orb-1"></div>
+      <div className="orb orb-2"></div>
+      <div className="orb orb-3"></div>
+      <div className="orb orb-4"></div>
+      <div className="orb orb-5"></div>
+    </div>
+  )
+}
+
+export default FloatingOrbs
